4e377e079ce7a0b4cfcb7d46c42d66d9716c983e
Daniel Copper <daniel.kueppermann@gmail.com>
2023-04-10 20:42:30 +0200

    Fixed Typo.

--- lua/copper/plugins/comments.lua
+++ lua/copper/plugins/comments.lua
@@ -71,7 +71,7 @@ return {
                 -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
             },
         },
-        key = {
+        keys = {
             vim.keymap.set('n', ']t', function() require('todo-comments').jump_next() end, { desc = 'Next todo comment' }),
             vim.keymap.set('n', '[t', function() require('todo-comments').jump_prev() end,
                 { desc = 'Previous todo comment' }),
