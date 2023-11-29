f12c966626c2c9184faa0046c13e4e29b18ad57e
Daniel Copper <daniel.kueppermann@gmail.com>
2023-11-29 19:05:15 +0100

    Switched back to markdownpreview because of dependencies

--- lua/copper/plugins/coding/markdown.lua
+++ lua/copper/plugins/coding/markdown.lua
@@ -1,14 +1,18 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
-    enabled = false,
+    enabled = true,
     build = function()
       vim.fn["mkdp#util#install"]()
     end,
     ft = "markdown",
+    config = function ()
+      vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":MarkdownPreviewToggle<cr>", { silent = true })
+    end,
   },
   {
     "wallpants/github-preview.nvim",
+    enabled = false,
     cmd = { "GithubPreviewToggle" },
     keys = { "<leader>mpt" },
     opts = {
