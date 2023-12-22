6232242ee1fedb7322c01443400b3b4d8c903f53
Daniel Kueppermann <daniel.kueppermann@l-p-a.com>
2023-12-22 10:05:04 +0100

    Added lsps to statusline

--- lua/copper/plugins/ui/statusline.lua
+++ lua/copper/plugins/ui/statusline.lua
@@ -74,6 +74,27 @@ return {
                 removed = icons.git.Removed,
               },
             },
+            {
+              function()
+                local msg = "LS Inactive"
+                local buf_clients = vim.lsp.get_active_clients()
+                if next(buf_clients) == nil then
+                  if type(msg) == "boolean" or #msg == 0 then
+                    return "LS Inactive"
+                  end
+                end
+                local buf_client_names = {}
+
+                for _, client in pairs(buf_clients) do
+                  table.insert(buf_client_names, client.name)
+                end
+
+                local unique_client_names = vim.fn.uniq(buf_client_names)
+                local language_servers = table.concat(unique_client_names, ", ")
+
+                return language_servers
+              end
+            },
             { "encoding" },
             { "fileformat" },
           },
