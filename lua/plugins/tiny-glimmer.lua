d7657417024f481c1e2917f8833bfb01f5172a1a
Daniel Küppermann <daniel.kueppermann@l-p-a.com>
2025-11-15 21:12:38 +0100

    feat: disable tiny-glimmer which breaks paste (p)

--- lua/plugins/tiny-glimmer.lua
+++ lua/plugins/tiny-glimmer.lua
@@ -1,5 +1,6 @@
 return {
     "rachartier/tiny-glimmer.nvim",
+    enabled = false, -- DISABLED: Blocks paste operations
     event = "VeryLazy",
     priority = 10, -- Low priority to catch other plugins' keybindings
     config = function()
