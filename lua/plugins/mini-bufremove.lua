2266db1bc83200f90bf664bea541c3e15d64633b
Daniel Kueppermann <daniel.kueppermann@gmail.com>
2026-03-18 20:58:10 +0100

    feat(buffers): add keybindings

--- lua/plugins/mini-bufremove.lua
+++ lua/plugins/mini-bufremove.lua
@@ -5,5 +5,18 @@ return {
   keys = {
     { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete buffer" },
     { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete buffer (force)" },
+    { "<leader>bda", function()
+      local br = require("mini.bufremove")
+      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.bo[buf].buflisted then br.delete(buf, false) end
+      end
+    end, desc = "Delete all buffers" },
+    { "<leader>bdo", function()
+      local br = require("mini.bufremove")
+      local cur = vim.api.nvim_get_current_buf()
+      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.bo[buf].buflisted and buf ~= cur then br.delete(buf, false) end
+      end
+    end, desc = "Delete other buffers" },
   },
 }
