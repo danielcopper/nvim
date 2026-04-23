0d5ed50dc17077522e5970583027d20649449357
Daniel Kueppermann <daniel.kueppermann@gmail.com>
2026-04-23 09:10:55 +0200

    fix(autocmds): remove forced line wrap for text filetypes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -138,7 +138,6 @@ vim.api.nvim_create_autocmd("FileType", {
   group = augroup("text_files"),
   pattern = { "gitcommit", "markdown", "text" },
   callback = function()
-    vim.opt_local.wrap = true
     vim.opt_local.spell = true
   end,
 })
